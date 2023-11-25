78029b563d22837001329a81f2d7d61fef4bf70d
min <rlaalswl1238@naver.com>
2023-11-25 23:38:57 +0900

    2023-11-25 rigidBody

---
+++ rigidBody/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1884036.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 158366556 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/158366543/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/158366544/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/158366542/1/ammo.js', 'preload' : true},
+];
